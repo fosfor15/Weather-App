64ab14283da7fded2384436c0fd1f5d2cf522ebd
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2022-04-28 21:20:55 +0300

    fix: Build config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [ react() ],
+    optimizeDeps: {
+        esbuildOptions: {
+            define: {
+                global: 'globalThis'
+            }
+        }
+    },
+    build: {
+        rollupOptions: {
+            plugins: [ rollupNodePolyFill() ]
+        }
+    },
     resolve: {
         alias: [
             {
