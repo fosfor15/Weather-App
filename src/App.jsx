c346f3bf19b1b1a04bc3c6fdb2ce9f4f3ad0d1c1
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2022-04-28 16:14:13 +0300

    style: Basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
-import './styles/App.css';
+import { View, Flex, Heading, SearchField, TextAreaField } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Template</h1>
-        </div>
+        <View>
+            <Flex
+                className="App"
+                direction="column"
+                gap="1rem"
+                style={styles.app}
+            >
+                <Heading
+                    level="1"
+                    fontWeight="400"
+                    textAlign="center"
+                    style={styles.heading}
+                >Weather App</Heading>
+
+                <SearchField
+                    placeholder="Enter city name"
+                />
+
+                <TextAreaField
+                    placeholder="The weather data will be here"
+                    isReadOnly={false}
+                    rows="10"
+                />
+            </Flex>
+        </View>
     )
 }
 
 export default App;
+
+const styles = {
+    app: {
+        width: '500px',
+        margin: '40px auto'
+    },
+    heading: {
+        marginBottom: '30px'
+    }
+};
