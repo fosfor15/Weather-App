6c776b8c4d03cfab27a54f9f263235dda32e1faa
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2022-04-29 10:57:40 +0300

    refactor: Improve App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,98 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+
 import { View, Flex, Heading, SearchField, TextAreaField } from '@aws-amplify/ui-react';
 import { API } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 
+
 function App() {
+    const [ city, setCity ] = useState('');
     const [ weatherData, setWeatherData ] = useState(null);
-    const cityNameInput = useRef(null);
-    const weatherOutputTextarea = useRef(null);
+    const [ weatherOutput, setWeatherOutput ] = useState('');
+
+    const changeCity = (event) => {
+        setCity(event.target.value);
+    };
+
+    const clearCity = () => {
+        setCity('');
+    };
     
     const getWeatherData = () => {
         const config = {
             response: true, 
-            queryStringParameters: {
-                city: cityNameInput.current.value
-            }
+            queryStringParameters: { city }
         };
 
-        cityNameInput.current.value = '';
+        setWeatherOutput('');
         
         API.get('getCurrentWeather', '/v1/getCurrentWeather/', config)
             .then(response => {
                 console.log('Response :>> ', response);
 
-                const newWeatherData = JSON.parse(response.data.response);
-                console.log('newWeatherData :>> ', newWeatherData);
+                const _weatherData = JSON.parse(response.data.response);
+                console.log('Weather data :>> ', _weatherData);
 
-                setWeatherData(newWeatherData);
+                setWeatherData(_weatherData);
             })
             .catch(error => {
                 console.log('Error :>> ', error);
             });
     };
 
-    useEffect(() => {
+    const displayWeatherData = () => {
         if (!weatherData) return;
 
-        let weatherOutput = `Weather for city ${weatherData.city}\n`;
-        weatherOutput += `Temperature: ${weatherData.temperature}°C\n`;
-        weatherOutput += `Weather conditions: ${weatherData.weatherCondition.type}\n`;
-        weatherOutput += `Wind: ${weatherData.wind.speed} km/h\n`;
-        weatherOutput += `Wind direction: ${weatherData.wind.direction}\n`;
-            /* .replace(/(\w)(\w)?/, (match, l1, l2) => {
-                if (l1 == 'N') {
-                    'North'
-                }
-            }); */
-        weatherOutput += `Pressure: ${weatherData.weatherCondition.pressure} Pa\n`;
-        weatherOutput += `Humidity: ${weatherData.weatherCondition.humidity}%`;
+        let _weatherOutput = `Weather for city ${city}\n\n`;
 
-        weatherOutputTextarea.current.value = weatherOutput;
-    }, [ weatherData ]);
+        _weatherOutput += `Weather conditions: ${weatherData.weatherCondition.type}\n`;
+        _weatherOutput += `Temperature: ${weatherData.temperature}°C\n`;
+        _weatherOutput += `Wind: ${weatherData.wind.speed} km/h\n`;
+
+        if (weatherData.wind.direction) {
+            _weatherOutput += `Wind direction: ${weatherData.wind.direction
+                .replace(/(\w)(\w)?/, (match, l1, l2) => {
+                    let output = match;
+    
+                    switch (l1) {
+                        case 'N':
+                            output += ' (North';
+                            break;
+                        case 'E':
+                            output += ' (East';
+                            break;
+                        case 'S':
+                            output += ' (South';
+                            break;
+                        case 'W':
+                            output += ' (West';
+                            break;
+                        }
+    
+                    switch (l2) {
+                        case 'E':
+                            output += ' East)';
+                            break;
+                        case 'W':
+                            output += ' West)';
+                            break;
+                        default:
+                            output += ')';
+                    }
+    
+                    return output;
+                })}\n`;
+        } else {
+            _weatherOutput += 'Wind direction: unknown\n'; 
+        }
+
+        _weatherOutput += `Pressure: ${weatherData.weatherCondition.pressure} Pa\n`;
+        _weatherOutput += `Humidity: ${weatherData.weatherCondition.humidity}%`;
+
+        setWeatherOutput(_weatherOutput);
+    };
+
+    useEffect(displayWeatherData, [ weatherData ]);
 
 
     return (
@@ -62,21 +105,22 @@ function App() {
             >
                 <Heading
                     level="1"
-                    fontWeight="400"
-                    textAlign="center"
                     style={styles.heading}
                 >Weather App</Heading>
 
                 <SearchField
                     placeholder="Enter city name"
-                    ref={cityNameInput}
+                    value={city}
+                    onChange={changeCity}
+                    onClear={clearCity}
                     onSubmit={getWeatherData}
                 />
 
                 <TextAreaField
-                    ref={weatherOutputTextarea}
-                    placeholder="The weather data will be here"
-                    isReadOnly={false}
+                    placeholder="The weather information will be placed here"
+                    value={weatherOutput}
+                    isReadOnly={true}
+                    whiteSpace="pre"
                     rows="10"
                 />
             </Flex>
@@ -86,12 +130,15 @@ function App() {
 
 export default App;
 
+
 const styles = {
     app: {
         width: '500px',
         margin: '40px auto'
     },
     heading: {
-        marginBottom: '30px'
+        marginBottom: '30px',
+        fontWeight: '400',
+        textAlign: 'center'
     }
 };
